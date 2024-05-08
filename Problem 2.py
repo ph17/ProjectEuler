8f41cb86862da35b6795f5192a71b8354f4be52a
Pedro Henrique <pedrohenriquelemos17@gmail.com>
2024-05-08 11:12:20 -0300

    small correction on the fibonnaci sequence of P2

--- Problem 2.py
+++ Problem 2.py
@@ -1,6 +1,6 @@
 def main():
     even_values = [2]
-    fibonacci = [1, 2]
+    fibonacci = [1, 1, 2]
     while fibonacci[-1] < 4000000:
         next_value = fibonacci[-1] + fibonacci[-2]
         if next_value % 2 == 0:
